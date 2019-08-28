444b7f67a78ad99b02d66b02db48ee9bdb90a218
BigETI <bigeti.ethem@gmail.com>
2019-08-28 13:37:27 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Parallel library for Unity
+
+## Description
+This package contains a parallel library for Unity, which is a wrapper of `System.Threading.Tasks.Parallel`.
+
+## License
+[The license](https://github.com/BigETI/UnityParallel/blob/master/LICENSE) for this package is available at https://github.com/BigETI/UnityParallel/blob/master/LICENSE
---
+++ Scripts/Static/Parallel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Unity parallel namespace
+/// </summary>
+namespace UnityParallel
+{
+    /// <summary>
+    /// Parallel class
+    /// </summary>
+    public static class Parallel
+    {
+        /// <summary>
+        /// Default minimum partition size
+        /// </summary>
+        private static readonly int defaultMinimumPartitionSize = 128;
+
+        /// <summary>
+        /// Default parallel options
+        /// </summary>
+        private static readonly ParallelOptions defaultParallelOptions = new ParallelOptions
+        {
+            CancellationToken = System.Threading.CancellationToken.None,
+            MaxDegreeOfParallelism = SystemInfo.processorCount,
+            TaskScheduler = TaskScheduler.Default
+        };
+
+        /// <summary>
+        /// Partitioned parallel "for" loop
+        /// </summary>
+        /// <param name="fromInclusive">From (inclusive)</param>
+        /// <param name="toExclusive">To (Exclusive)</param>
+        /// <param name="minimumPartitionSize">Minimum partition size</param>
+        /// <param name="body">Body</param>
+        public static void For(int fromInclusive, int toExclusive, int minimumPartitionSize, Action<int> body)
+        {
+            int length = (toExclusive - fromInclusive);
+            if ((body != null) && (length > 0))
+            {
+                int partition_size = length / SystemInfo.processorCount;
+                if ((SystemInfo.processorCount > 1) && (minimumPartitionSize <= partition_size))
+                {
+                    if (toExclusive > fromInclusive)
+                    {
+                        System.Threading.Tasks.Parallel.ForEach(Partitioner.Create(fromInclusive, toExclusive, partition_size), defaultParallelOptions, (partition) =>
+                        {
+                            for (int i = partition.Item1; i < partition.Item2; i++)
+                            {
+                                body(i);
+                            }
+                        });
+                    }
+                }
+                else
+                {
+                    for (int i = fromInclusive; i < toExclusive; i++)
+                    {
+                        body(i);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Automatically partitioned parallel "for" loop
+        /// </summary>
+        /// <param name="fromInclusive">From (inclusive)</param>
+        /// <param name="toExclusive">To (exclusive)</param>
+        /// <param name="body">Body</param>
+        public static void For(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            For(fromInclusive, toExclusive, defaultMinimumPartitionSize, body);
+        }
+
+        /// <summary>
+        /// Partitioned parallel "foreach" loop
+        /// </summary>
+        /// <typeparam name="T">Array element type</typeparam>
+        /// <param name="array">Array</param>
+        /// <param name="minimumPartitionSize">Minimum partition size</param>
+        /// <param name="body">Body</param>
+        public static void ForEach<T>(IReadOnlyList<T> array, int minimumPartitionSize, Action<T> body)
+        {
+            if ((array != null) && (body != null))
+            {
+                For(0, array.Count, minimumPartitionSize, (i) =>
+                {
+                    body(array[i]);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Automatically partitioned parallel "foreach" loop
+        /// </summary>
+        /// <typeparam name="T">Array element type</typeparam>
+        /// <param name="array">Array</param>
+        /// <param name="body">Body</param>
+        public static void ForEach<T>(IReadOnlyList<T> array, Action<T> body)
+        {
+            ForEach(array, defaultMinimumPartitionSize, body);
+        }
+    }
+}
